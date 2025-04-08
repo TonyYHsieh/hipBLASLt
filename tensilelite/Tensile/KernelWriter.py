6e2df5a2e89bf202bd81801e7c81218921ea8470
yu-hsieh <Yu-cheng.Hsieh@amd.com>
2025-04-08 02:50:41 +0000

    MX: staggerU

--- tensilelite/Tensile/KernelWriter.py
+++ tensilelite/Tensile/KernelWriter.py
@@ -2006,8 +2006,12 @@ class KernelWriter(metaclass=abc.ABCMeta):
     if not forceNoTileCode:
       module.add(self.declareStaggerParms(kernel))
       module.add(self.calculateStagger(kernel, tensorParametersA))
+      if kernel["ProblemType"]["MXBlockA"]:
+        module.add(self.calculateStagger(kernel, tensorParametersA["MX"]))
       if kernel["ProblemType"]["Sparse"] and not kernel["DirectToVgprSparseMetadata"]:
         module.add(self.calculateStagger(kernel,tPM))
+      if kernel["ProblemType"]["MXBlockB"]:
+        module.add(self.calculateStagger(kernel, tensorParametersB["MX"]))
       module.add(self.calculateStagger(kernel, tensorParametersB))
 
     # LRO and LWA as assigned
